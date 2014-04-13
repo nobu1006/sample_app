fbee024fc50796a2abda4aac6b884393d086837c
Nobuhiko Tobita <nobu1006@gmail.com>
2014-04-13 22:20:16 +0900

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   get "users/new"
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,11 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
